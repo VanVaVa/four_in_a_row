a6d4bf607d687771f0a7d6538cd969ad3a440a58
Van <MisterDeterminator@yandex.ru>
2025-10-24 10:37:53 +0000

    feat: added column pointer

--- src/widgets/board/Board.tsx
+++ src/widgets/board/Board.tsx
@@ -1,18 +1,29 @@
+import { useState } from "react";
 import { useGameStore } from "../../entities/game/useGameStore";
 import Chip from "../../shared/components/chip/Chip";
 import styles from "./board.module.css";
 
 const Board = () => {
   const { getBoard, move, winData } = useGameStore();
+  const [activeColumn, setActiveColumn] = useState<number | null>(null);
 
   return (
     <div className={styles.board}>
+      <div
+        style={{
+          display: activeColumn === null ? "none" : "block",
+          left: 80 * (activeColumn || 0),
+        }}
+        className={styles.columnPointer}
+      />
       {getBoard().map((col, colIdx) => (
         <button
           disabled={!!winData?.length}
           key={colIdx}
           className={styles.column}
           onClick={() => move(colIdx)}
+          onMouseEnter={() => setActiveColumn(colIdx)}
+          onMouseLeave={() => setActiveColumn(null)}
         >
           {col.map((el, idx) => (
             <Chip
